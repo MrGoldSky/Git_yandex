5cda0676798a9d719753bcb124fc792b600d795e
MrGoldSky <ilia19042006@gmail.com>
2023-02-10 19:33:10 +0300

    Тренировки в полёте

--- Flask/main.py
+++ Flask/main.py
@@ -9,5 +9,12 @@ def index(title):
     param['title'] = f"{title}"
     return render_template('index.html', **param)
 
+@app.route("/training/<prof>")
+def traning(prof):
+    param = {}
+    param['simulator'] = {prof}
+    return render_template('index.html', **param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
