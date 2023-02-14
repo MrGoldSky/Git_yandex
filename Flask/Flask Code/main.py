039d1aa3083d140092aba3987a2d5a9399e64ea6
MrGoldSky <ilia19042006@gmail.com>
2023-02-14 18:35:19 +0300

    Варианты выбора

--- Flask/Flask Code/main.py
+++ Flask/Flask Code/main.py
@@ -30,6 +30,30 @@ def list_prof(listy):
     return render_template('index.html', **param)
 
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" href="/static/css/style.css'" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предположение: {planet_name}</h1>
+                    <p>Это планета близка к Земле;</p>
+                      <div class="alert alert-secondary alert" role="alert">На планете была вода! </div>
+                      <div class="alert alert-primary alert" role="alert">У планеты нет почвы </div>
+                      <div class="alert alert-warning alert" role="alert">На планете нет кислорода </div>
+                    
+                  </body>
+                </html>"""
+
+
 @app.route('/promotion')
 def promotion():
     countdown_list = ['Человечество вырастает из детства.',
