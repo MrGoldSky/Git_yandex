66c4f0507b954af851dfafe120b486346c786bfe
Ilia Rosseev <ilia19042006@gmail.com>
2023-02-14 22:18:32 +0300

    Результат отбора

--- Flask/Flask Code/main.py
+++ Flask/Flask Code/main.py
@@ -79,10 +79,36 @@ def image_mars():
                   </head>
                   <body>
                     <h1>Жди нас, Марса</h1>
-                    <img src="/static/img/mars.jpg" alt="Марс потерялся">
+                    <img src="/static/img/mars1.jpg" alt="Марс потерялся">
                     <p>Это марс.</p>
                   </body>
                 </html>"""
+
+
+@app.route('/rating/<nickname>/<int:level>/<float:rating>')
+def rating(nickname, level, rating):
+  return f'''
+              <!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" href="/static/css/style.css'" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Результат отборас</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <p>Пренендента на участие в миссии {nickname}</p>
+                      <div class="alert alert-primary alert" role="alert">Поздравляем! Ваш рейтинг после {level} этапа отбора составляет {rating} </div>
+                      <div class="alert alert-warning alert" role="alert">Желаем удачи </div>
+                      
+                    
+                  </body>
+                </html>
+              '''
 
 
 @app.route('/promotion_image')
@@ -101,7 +127,7 @@ def promotion_image():
                     <body>
                       <h1>Жди нас, Марс</h1>
                       </br>
-                      <img src="/static/img/mars.jpg" alt="Марс потерялся">
+                      <img src="/static/img/mars1.jpg" alt="Марс потерялся">
                       <div class="alert alert-warning alert" role="alert">'Человечество вырастает из детства.' </div>
                       <div class="alert alert-dark aler" role="alert">"Человечеству мала одна планета." </div>
                       <div class="alert alert-primary" role="alert">"Мы сделаем обитаемыми безжизненные пока планеты." </div>
