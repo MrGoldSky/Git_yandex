1f949ca4e4c2288a6b52d5b2a49d43706b51a2f2
MrGoldSky <ilia19042006@gmail.com>
2023-02-14 19:24:21 +0300

    Загрузка файла

--- Flask/Flask Code/main.py
+++ Flask/Flask Code/main.py
@@ -183,5 +183,36 @@ def form_sample():
                         </html>'''
 
 
+@app.route('/loadphoto', methods=['POST', 'GET'])
+def load_photo():
+  if request.method == 'GET':
+      return f'''<!doctype html>
+                      <html lang="en">
+                        <head>
+                          <meta charset="utf-8">
+                          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                          <link rel="stylesheet"
+                          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                          crossorigin="anonymous">
+                          <link rel="stylesheet" href="/static/css/style.css" />
+                          <title>Загрузка фото</title>
+                        </head>
+                        <body>
+                        <img src='/image.png')>
+                          <form method="post" enctype="multipart/form-data">
+                            <h1 class="form_h1" >Загрузка фото</h1>
+                              <div class="form-group">
+                                  <label for="photo">Приложите фотографию</label>
+                                  <input type="file" class="form-control-file" id="photo" name="file">
+                              </div>
+                                  <button type="submit" class="btn btn-primary">Загрузить</button>
+                          </form>
+                        </body>
+                      </html>'''
+  elif request.method == 'POST':
+      f = request.files['file']
+      f.save("image.png")
+      return "Форма отправлена"
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
