b010c99c2a56a38a630f6d92e328c14c86a08ad5
MrGoldSky <ilia19042006@gmail.com>
2023-02-21 18:28:10 +0300

    news.py

--- Flask/Flask_SQL/data/users.py
+++ Flask/Flask_SQL/data/users.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
 
 
 class User(SqlAlchemyBase):
@@ -15,3 +16,4 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
                                      default=datetime.datetime.now)
+    news = orm.relationship("News", back_populates='user')
